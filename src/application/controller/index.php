7090aec87c8689eea348d414b8c9bd2a59357db6
ghost <noreply@localhost>
2021-12-28 19:32:42 +0200

    set login page as project home

--- src/application/controller/index.php
+++ src/application/controller/index.php
@@ -1,3 +1,5 @@
 <?php
 
-require(PROJECT_DIR . '/application/view/index.phtml');
+//require(PROJECT_DIR . '/application/view/index.phtml');
+
+require(PROJECT_DIR . '/application/controller/login.php');
