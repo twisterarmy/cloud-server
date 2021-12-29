55b8362246cf2b2c4d8c1939d9eed382403c4b22
ghost <noreply@localhost>
2021-12-29 19:20:51 +0200

    move network status to the header

--- src/application/controller/common/footer/guest.php
+++ src/application/controller/common/footer/guest.php
@@ -1,6 +1,3 @@
 <?php
 
-$usersTotal  = $_modelUser->getTotal();
-$blocksTotal = $_modelBlock->getTotal();
-
 require(PROJECT_DIR . '/application/view/common/footer/guest.phtml');
--- src/application/controller/common/header/user.php
+++ src/application/controller/common/header/user.php
@@ -1,3 +1,6 @@
 <?php
 
+$usersTotal  = $_modelUser->getTotal();
+$blocksTotal = $_modelBlock->getTotal();
+
 require(PROJECT_DIR . '/application/view/common/header/user.phtml');
