f5f294bc9089e900ac44b4aa5714232a35de9e33
ghost <noreply@localhost>
2021-12-30 20:38:16 +0200

    init follow api

---
+++ src/application/controller/api/follow/total.php
@@ -0,0 +1,36 @@
+<?php
+
+$response = [
+  'success' => false,
+  'message' => _('Internal server error'),
+  'total'   => 0
+];
+
+if (isset($_SESSION['userName'])) {
+
+  $userName = isset($_POST['userName']) ? $_POST['userName'] : $_SESSION['userName'];
+
+  $followingUsersTotal = 0;
+
+  foreach ((array) $_twister->getFollowing($_SESSION['userName']) as $followingUserName) {
+    $followingUsersTotal++;
+  }
+
+  $response = [
+    'success' => true,
+    'message' => _('Follow totals received'),
+    'total'   => $followingUsersTotal
+  ];
+
+} else {
+
+  $response = [
+    'success' => false,
+    'message' => _('Session expired. Please, reload the page.'),
+    'total'   => 0
+  ];
+
+}
+
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($response);
