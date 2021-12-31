a06afb19cdaf0b971c9f56e6b39d90b6e3f2bee3
ghost <noreply@localhost>
2021-12-31 06:08:27 +0200

    implement avatar image cache

--- src/application/controller/api/image.php
+++ src/application/controller/api/image.php
@@ -2,8 +2,20 @@
 
 if (isset($_GET['hash'])) {
 
-  $icon = new Icon();
+  $filename = md5($_GET['hash']);
+  $filepath = PROJECT_DIR . '/cache/image/' . $filename . '.jpeg';
+
+  if (!file_exists($filepath)) {
+
+    $icon  = new Icon();
+    $image = $icon->generateImageResource($filename, 42, 42, false);
+
+    file_put_contents($filepath, $image);
+  }
+
+  $image = file_get_contents($filepath);
 
   header("Content-Type: image/jpeg");
-  echo $icon->generateImageResource(md5($_GET['hash']), 42, 42, false);
+
+  echo $image;
 }
