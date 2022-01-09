5ef188e19cec58fc94926b47298d1e07821d2a6c
ghost <noreply@localhost>
2022-01-09 20:52:07 +0200

    fix 404 page

--- src/application/controller/error/404.php
+++ src/application/controller/error/404.php
@@ -4,4 +4,8 @@ header('HTTP/1.0 404 Not Found');
 
 $metaTitle = _('Page not Found | Twisterarmy Cloud');
 
+// Load dependencies
+$metaStyles  = ['css/template/default/error/404.css',];
+$metaScripts = [];
+
 require(PROJECT_DIR . '/application/view/error/404.phtml');
